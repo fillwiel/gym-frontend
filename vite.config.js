640544d51c0863f7b99f8eb1e64abe55dc0cd752
Filip Wielkopolan <filip.wielkopolan@gmail.com>
2026-02-26 00:15:57 +0100

    Revert "Updated ingress rules to map application to /gym path."
    This reverts commit 1d91edcbe93451e16da9e5ebaae4d4beac137a8a.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/gym',
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
